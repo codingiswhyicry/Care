d5125262b1bc8b3518188e53c5b545726e488774
Amanda Southworth <amanda.southworth@myds.us>
2017-06-06 08:29:47 -0700

    added base wireframing

---
+++ Care++/Care++/priorityFour.swift
@@ -0,0 +1,24 @@
+//
+//  priorityFour.swift
+//  Care++
+//
+//  Created by Amanda Southworth on 6/5/17.
+//  Copyright © 2017 Amanda Southworth. All rights reserved.
+//
+
+import UIKit
+
+class priorityFour: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
