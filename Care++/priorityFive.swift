7a5dc4a65e0e973d76cb14929df1ca3f539b6a05
Amanda Southworth <amanda.southworth@myds.us>
2017-06-06 10:14:07 -0700

    added different UI elements

--- Care++/priorityFive.swift
+++ Care++/priorityFive.swift
@@ -1,14 +1,14 @@
 //
-//  priorityTwo.swift
+//  priorityFive.swift
 //  Care++
 //
-//  Created by Amanda Southworth on 6/5/17.
+//  Created by Amanda Southworth on 6/6/17.
 //  Copyright © 2017 Amanda Southworth. All rights reserved.
 //
 
 import UIKit
 
-class priorityTwo: UITableViewCell {
+class priorityFive: UITableViewCell {
 
     override func draw(_ rect: CGRect) {
         
