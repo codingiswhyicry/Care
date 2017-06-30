d06a15a1714924deb0b7ab76e69089e230a71425
Amanda Southworth <amanda.southworth@myds.us>
2017-06-29 22:53:54 -0700

    ahhhhhh

--- Care++/Care++/ViewController.swift
+++ Care++/Care++/ViewController.swift
@@ -15,11 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var taskTable: UITableView!
+    
 
 }
 
