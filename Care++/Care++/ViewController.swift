9310afc8e951040a63f908079491bfcf0df4f799
Amanda Southworth <amandasouthworth@Amandas-MacBook-Pro.local>
2017-06-30 17:53:57 -0700

    yay

--- Care++/Care++/ViewController.swift
+++ Care++/Care++/ViewController.swift
@@ -8,12 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class firstPriority: UITableViewCell {
+
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+    @IBOutlet weak var taskTitle: UILabel!
+    @IBOutlet weak var separator: UIView!
+    
+}
+
+class secondPriority: UITableViewCell {
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+    @IBOutlet weak var taskTitle: UILabel!
+    @IBOutlet weak var separator: UIView!
+    
+}
+
+class thirdPriority: UITableViewCell {
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+    @IBOutlet weak var taskTitle: UILabel!
+    @IBOutlet weak var separator: UIView!
+
+}
+
+class fourthPriority: UITableViewCell {
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+    @IBOutlet weak var taskTitle: UILabel!
+    @IBOutlet weak var separator: UIView!
+    
+    
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected row is \(indexPath.row)")
+    }
+    
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "firstCell", for: indexPath) as! firstPriority
+    
+    cell.taskTitle.text = "\(indexPath.row)"
+    
+    return cell
+    
+  }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        taskTable.delegate = self
+        taskTable.dataSource = self
+        taskTable.backgroundColor = UIColor.clear
     }
+    
+    
 
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var taskTable: UITableView!
